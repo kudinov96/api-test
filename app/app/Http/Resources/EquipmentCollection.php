a351c29d212f66b27716d1fa7e56332fafa73eea
kudinov <kudinov@rutorika.ru>
2022-04-15 18:09:46 +0300

    Change response

---
+++ app/app/Http/Resources/EquipmentCollection.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Resources\Json\ResourceCollection;
+
+class EquipmentCollection extends ResourceCollection
+{
+    /**
+     * The resource that this resource collects.
+     *
+     * @var string
+     */
+    public $collects = EquipmentResource::class;
+
+    /**
+     * Transform the resource collection into an array.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return array|\Illuminate\Contracts\Support\Arrayable|\JsonSerializable
+     */
+    public function toArray($request)
+    {
+        return [
+            "success" => true,
+            "result" => $this->collection,
+        ];
+    }
+}
